270989b5745d6ecbc371f9bbca540412d7634359
Ronald Silva Alves <ronald.alves@NMAC-3420.local>
2022-11-24 08:34:40 -0300

    feat: create DismissKeyBoard component

---
+++ src/components/DismissKeyBoard/DismissKeyBoard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TouchableWithoutFeedback, Keyboard } from "react-native";
+
+const DismissKeyBoard = ({ children }: { children: JSX.Element }) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        {children}
+    </TouchableWithoutFeedback>
+);
+
+export default DismissKeyBoard;
